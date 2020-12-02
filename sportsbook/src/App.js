dc573806d413843789f29622eaf982a7e79ca6e4
Aaron Williams <aaronrw4@yahoo.com>
2020-12-01 16:14:09 -0800

    Added header to App.js

--- sportsbook/src/App.js
+++ sportsbook/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Components/header';
 import GameLines from './Components/gameLines';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <GameLines/>
     </div>
   );
