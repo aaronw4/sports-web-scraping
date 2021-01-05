d59c1bdeb6a5dea9d29fdaf7c151e2c5d9c2c80a
Aaron Williams <aaronrw4@yahoo.com>
2021-01-05 00:43:58 -0800

    Created js fxn to call scraping.py

---
+++ sportsbook/src/scraping function/callScraping.js
@@ -0,0 +1,15 @@
+function callPython() { 
+    var spawn = require("child_process").spawn; 
+     
+    var process = spawn('python', ["c:/Users/aaron/Git/sports-web-scraping/sportsbook/src/scraping function/scraping.py"]); 
+  
+    process.stdout.on('data', function() { 
+        console.log('Scraping complete'); 
+    }) 
+
+    process.stderr.on('data', function() { 
+        console.log('Scraping error'); 
+    }) 
+} 
+
+callPython()
